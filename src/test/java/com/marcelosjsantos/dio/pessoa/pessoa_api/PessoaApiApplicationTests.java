5b5d12f7f6307fda3874df4fb912e1c7a59a0ade
Marcelo JS Santos <marcelojssantos2012@gmail.com>
2021-04-30 17:53:09 -0300

    Criação do proejeto
    - Importação do proejto feito no Spring Initializr
    - Configurações Iniciais
      - SDK
      - BookController
      - system.properties
    - Criação class PessoaController ("API Test!)

---
+++ src/test/java/com/marcelosjsantos/dio/pessoa/pessoa_api/PessoaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcelosjsantos.dio.pessoa.pessoa_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PessoaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

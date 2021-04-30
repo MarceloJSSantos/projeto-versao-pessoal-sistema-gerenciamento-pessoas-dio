e7cc7fc171ef689c95ff6260b56572f0b41fbca6
Marcelo JS Santos <marcelojssantos2012@gmail.com>
2021-04-30 18:00:46 -0300

    Acerto do mapeamento
    - "api/vi/pessoas" p/ "api/v1/pessoas"

--- src/main/java/com/marcelosjsantos/dio/pessoa/pessoa_api/controller/PessoaController.java
+++ src/main/java/com/marcelosjsantos/dio/pessoa/pessoa_api/controller/PessoaController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/vi/pessoas")
+@RequestMapping("api/v1/pessoas")
 public class PessoaController {
 
     @GetMapping

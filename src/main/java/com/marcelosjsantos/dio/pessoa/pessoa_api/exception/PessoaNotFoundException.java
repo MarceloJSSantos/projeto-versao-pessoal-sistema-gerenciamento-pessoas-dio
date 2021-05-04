75b31c9b46be3f0f5f62ef4472551de5900c0f41
Marcelo JS Santos <marcelojssantos2012@gmail.com>
2021-05-04 19:44:11 -0300

    Acerto mensagem se Id não existir
    - acerto da mensagem para compatibilidade c/ Heroku

--- src/main/java/com/marcelosjsantos/dio/pessoa/pessoa_api/exception/PessoaNotFoundException.java
+++ src/main/java/com/marcelosjsantos/dio/pessoa/pessoa_api/exception/PessoaNotFoundException.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class PessoaNotFoundException extends Exception {
     public PessoaNotFoundException(Long id) {
-        super("A pessoa com o Id "+ id +" não foi encontrada!");
+        super(String.format("A pessoa com o Id "+ id +" não foi encontrada!"));
     }
 }
